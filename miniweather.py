b65024486e468284e8200c7da3778f894bb250ba
Ryan Tucker <rtucker@gmail.com>
2009-07-03 13:07:54 -0400

    adding support for conditions forecasts (e.g. mostly cloudy)

--- miniweather.py
+++ miniweather.py
@@ -15,6 +15,8 @@ def getweather():
 	lowtempcarry = False
 	hightempcarry = False
 	popcarry = False
+	dayconditionscarry = False
+	nightconditionscarry = False
 	
 	for i in forecast:
 		firstword = i.strip().split(' ')[0].strip('.')
@@ -24,6 +26,10 @@ def getweather():
 			forecastdatetime = datetime.date.today() + datetime.timedelta(dayoffset)
 			hightempnext = lowtempnext = False
 			hightemp = lowtemp = pop = False
+			dayconditions = nightconditions = False
+			conditions = i.strip().split('...')[1].split(',')[0].strip()
+			if conditions.split(' ')[0] in ['high', 'High', 'low', 'Low']:
+				conditions = False
 			for j in i.strip().split(' '):
 				if hightempnext:
 					hightempnext = False
@@ -33,36 +39,46 @@ def getweather():
 					lowtemp = int(j.strip(',.'))
 				elif j in ['high', 'High']:
 					hightempnext = True
+					dayconditions = conditions
 				elif j in ['low', 'Low']:
 					lowtempnext = True
+					nightconditions = conditions
 				elif j[-1] == '%':
 					pop = int(j[:-1])
 
 			if hightemp and lowtemp:
-				forecastdict[dayoffset] = (hightemp, lowtemp, pop)
+				forecastdict[dayoffset] = (hightemp, lowtemp, pop, dayconditions, nightconditions)
 			elif hightemp and lowtempcarry:
 				if popcarry:
 					pop = max(pop, popcarry)
-				forecastdict[dayoffset] = (hightemp, lowtempcarry, pop)
+				if nightconditionscarry:
+					nightconditions = nightconditionscarry
+					nightconditionscarry = False
+				forecastdict[dayoffset] = (hightemp, lowtempcarry, pop, dayconditions, nightconditions)
 				lowtempcarry = False
 			elif lowtemp and hightempcarry:
 				if popcarry:
 					pop = max(pop, popcarry)
-				forecastdict[dayoffset] = (hightempcarry, lowtemp, pop)
+				if dayconditionscarry:
+					dayconditions = dayconditionscarry
+					dayconditionscarry = False
+				forecastdict[dayoffset] = (hightempcarry, lowtemp, pop, dayconditions, nightconditions)
 				hightempcarry = False
 			elif lowtemp:
 				if dayoffset == 0:
-					forecastdict[dayoffset] = (False, lowtemp, pop)
+					forecastdict[dayoffset] = (False, lowtemp, pop, False, nightconditions)
 				else:
 					lowtempcarry = lowtemp
 					popcarry = pop
+					nightconditionscarry = nightconditions
 			elif hightemp:
 				hightempcarry = hightemp
+				dayconditionscarry = dayconditions
 				popcarry = pop
 	if hightempcarry:
 		# handle the "last day" of the forecast, which seems
 		# to only show a high temp.  experimental
-		forecastdict[dayoffset] = (hightempcarry, False, False)
+		forecastdict[dayoffset] = (hightempcarry, False, False, dayconditionscarry, nightconditions)
 
 	return forecastdict
 
