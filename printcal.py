290a39bc90b79cb8382692954b98b3f67a59ebbe
Ryan Tucker <rtucker@gmail.com>
2009-09-29 22:59:33 -0400

    adding a few random todo list items to the bottom

--- printcal.py
+++ printcal.py
@@ -91,6 +91,21 @@ def iter_todo(path, start=datetime.now(tzlocal()).replace(hour=0, minute=0,
             # empty list
             yield []
 
+def iter_random_todo(path):
+    """Returns iterator of random, no-due-date todo list items"""
+
+    todoids = os.popen(path + ' --due after/forever --task-ids-only').readlines()
+    if len(todoids) > 0:
+        random.shuffle(todoids)
+        for i in todoids:
+            try:
+                todoitem = os.popen(path + ' listid ' + i).readlines()[2]
+                line = re.sub(' \[.*\]$', '', todoitem.strip())
+                if line.startswith('*'):
+                    yield line
+            except:
+                yield 'Bad ID ' + i
+
 def iter_days(gcal, start=datetime.now(tzlocal()).replace(hour=0, minute=0,
               second=0, microsecond=0),
               end=datetime.fromtimestamp(2**31-86400, tz=tzlocal()),
@@ -238,7 +253,7 @@ def main():
 
     gcal = gcalcli.GoogleCalendar(username=usr, password=pwd, access=access, details=details)
 
-    footer = []
+    footer = ['']
 
     cookie = random.sample(open(cookiefile, 'r').readlines(), 1)[0].strip()
 
@@ -261,20 +276,33 @@ def main():
 
     while remaining > 0:
         row = iter.next()
-        if len(row) > 0:
+        if len(row) > 1:
             row.append('')  # to get a nice blank line
-        remaining -= len(row)
-        if remaining > 0:
-            for i in row:
-                if len(out) is (maxlength/3):
-                    # pad the line with dots if it's a good fold point
-                    out.append(str('{0:.<%i}' % maxwidth).format(i))
-                else:
-                    out.append(i)
+            remaining -= len(row)
+            if remaining > 0:
+                for i in row:
+                    if len(out) is (maxlength/3):
+                        # pad the line with dots if it's a good fold point
+                        out.append(str('{0:.<%i}' % maxwidth).format(i))
+                    else:
+                        out.append(i)
 
-    for i in range(0,maxlength-len(out)):
-        # pad with some blank lines
-        out.append('')
+    # reset remaining
+    remaining = maxlength - len(footer) - len(out)
+
+    if remaining > 2:
+        out.append('Todo List Items of the Future...')
+        remaining -= 1
+        todoiter = iter_random_todo(path='/home/rtucker/bin/todo.py')
+
+        while remaining > 0:
+            try:
+                row = textwrap.wrap(todoiter.next(), maxwidth)
+                if len(row) < remaining:
+                    remaining -= len(row)
+                    out.extend(row)
+            except StopIteration:
+                remaining = 0
 
     out.extend(footer)
 
